6105223fa1c1e5aabef291a968e67465dd1bb1fd
Shekhar <pythonic@gmail.com>
2011-06-20 16:31:28 +0530

    default config

---
+++ conf_default.py
@@ -0,0 +1,16 @@
+config = {
+    'pg_uri': 'dbname=shon',
+    'random_str': 'PLEASE_CHANGE',
+    'mail': {
+        'mail.on': True,
+        'mail.transport': 'smtp',
+        'mail.smtp.server': 'localhost',
+        'mail.smtp.port': 25,
+        'mail.smtp.tls': False,
+        'mail.smtp.username': '',
+        'mail.smtp.password': '',
+        'mail.smtp.debug': False,
+        'mail.utf8qp.on': True
+        },
+    'http_baseurl': 'http://127.0.0.1:5000'
+}
