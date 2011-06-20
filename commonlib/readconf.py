aa70bc872bccf49ea1540f3d59044f49223de887
Shekhar <pythonic@gmail.com>
2011-06-20 16:20:32 +0530

    config loading

---
+++ commonlib/readconf.py
@@ -0,0 +1,19 @@
+import copy
+import conf_default as default
+import commonlib.helpers
+
+odict = commonlib.helpers.odict
+
+def parse_config(conf_default, conf_local=None):
+    config = odict(**copy.deepcopy(default.config))
+    if conf_local:
+        local_config = conf_local.config
+        for section in config:
+            local_config_section = local_config.get(section)
+            if local_config_section:
+                if isinstance(local_config_section, dict):
+                    config[section].update(local_config_section)
+                else:
+                    config[section] = local_config_section
+    return config
+
