7b090f283077bcaf0453d01c8a9c9a2c5f94a493
Shekhar <pythonic@gmail.com>
2011-07-18 18:59:33 +0530

    merged state tests with be libs tests

--- tests/test_be_libs.py
+++ tests/test_be_libs.py
@@ -1,4 +1,10 @@
+import commonlib.shared.states
 import be.libs.macros as macroslib
+
+class dummy_obj_state(commonlib.shared.states.states):
+    enabled = 0
+    available = 1
+    hidden = 2
 
 def add_a_b(context, data, macro_data):
     return str(data['a'] + data['b'])
@@ -18,3 +24,17 @@ def test_macro_processing():
 def test_has_macro():
     text = 'HAS NO MACRO'
     assert macroslib.has_macro(text) == False
+
+def test_fromflags():
+    state_flags = 5
+    state_dict = dummy_obj_state.to_dict(state_flags)
+    assert state_flags == dummy_obj_state.to_flags(state_dict)
+    state_flags = 2
+    assert state_flags != dummy_obj_state.to_flags(state_dict)
+
+def test_fromdict():
+    state_dict = dict(enabled=False, available=True, hidden=False)
+    state_flags = dummy_obj_state.to_flags(state_dict)
+    assert state_dict == dummy_obj_state.to_dict(state_flags)
+    state_dict['hidden'] = True
+    assert state_dict != dummy_obj_state.to_dict(state_flags)
