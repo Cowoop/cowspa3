6aad93d622681ccc7cadf1498aac7f79dd7d982e
Shekhar <pythonic@gmail.com>
2011-08-10 20:07:11 +0530

    assigning role (and hence permissions)

--- be/apis/role.py
+++ be/apis/role.py
@@ -1,3 +1,4 @@
+import itertools
 import datetime
 import be.repository.access as dbaccess
 import commonlib.shared.roles as roledefs
@@ -5,6 +6,7 @@ import commonlib.shared.roles as roledefs
 biz_store = dbaccess.stores.biz_store
 bizplace_store = dbaccess.stores.bizplace_store
 userrole_store = dbaccess.stores.userrole_store
+userpermission_store = dbaccess.stores.userpermission_store
 
 def assign(user_id, roles, context=None):
     if not set(roles).issubset(roledefs.all_roles.keys()):
@@ -16,7 +18,16 @@ def assign(user_id, roles, context=None):
     roles_to_add = set(new_roles).difference(existing_roles)
     if roles_to_add:
         userrole_store.add_many([dict(user_id=user_id, role=role) for role in roles_to_add])
+
     # now assign permissions
+    perms = itertools.chain(*([p.name for p in roledefs.all_roles[role].permissions] for role in roles))
+    if context:
+        perms = [(context + '::' + p) for p in perms]
+    existing_perms = (row[0] for row in userpermission_store.get_by(crit=dict(user_id=user_id), fields=['permission'],
+        hashrows=False))
+    perms_to_add = set(perms).difference(existing_perms)
+    if perms_to_add:
+        userpermission_store.add_many([dict(user_id=user_id, permission=p) for p in perms_to_add])
     return True
 
 def revoke(user_id, roles, context=None):
