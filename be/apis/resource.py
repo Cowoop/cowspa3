ee137041c62af14454e7a4b7a5f2f277336bc2f6
Gaurav Rai <gaurav.hub@gmail.com>
2011-08-11 18:04:32 +0530

    Some Minor additions

--- be/apis/resource.py
+++ be/apis/resource.py
@@ -8,9 +8,9 @@ resourcerelation_store = dbaccess.stores.resourcerelation_store
 
 class ResourceCollection:
 
-    def new(self, name, owner, short_description, type, long_description=None):
+    def new(self, name, short_description, type, owner=1, long_description=None, time_based=True, quantity_unit=None):
         created = datetime.datetime.now()
-        data = dict(name=name, owner=owner, created=created, short_description=short_description, long_description=long_description, type=type)
+        data = dict(name=name, owner=owner, created=created, short_description=short_description, long_description=long_description, type=type, time_based=time_based, quantity_unit=quantity_unit)
         res_id = resource_store.add(**data)
 
         return res_id
