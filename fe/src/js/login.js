f6340858b0e28363b1d63efcb3f819f2ffeaad8f
Gaurav Rai <gaurav.hub@gmail.com>
2011-08-11 18:07:48 +0530

    Login page using knockout.js

--- fe/src/js/login.js
+++ fe/src/js/login.js
@@ -1,12 +1,15 @@
 H5F.setup(document.getElementById('login_form'));
-
-$('#login-btn').click(function () {
-    var params = {'username': $('#username').val(), password: $('#password').val()};
-    function success() {
-        $('#login-msg').html("<big>☑</big> Login is successful.");
-    };
-    function error() {
-        $('#login-msg').html("<big>Authentication Error. Try again</big>");
-    };
-    jsonrpc('login', params, success, error);
-});
+var viewControl  = {
+                username : ko.observable(), 
+                password : ko.observable(),
+                clicked :   function () {
+                            var params = {'username': this.username(), 'password': this.password()};
+                            function success() {
+                                $('#login-msg').html("<big>☑</big> Login is successful.");
+                                };
+                            function error() {
+                                $('#login-msg').html("<big>Authentication Error. Try again</big>");
+                                };
+                            jsonrpc('login', params, success, error);
+                }};
+ko.applyBindings(viewControl);
--- fe/src/pages.py
+++ fe/src/pages.py
@@ -8,14 +8,14 @@ class LoginPage(fe.bases.CSAnonPage):
     title = "Cowspa | Login"
     def main(self):
         container = tf.DIV()
-        username = tf.INPUT(type="TEXT", id='username', name="username", placeholder="Username")
-        password = tf.INPUT(type="PASSWORD", id='password', name="password", placeholder="Password")
-        submit = tf.BUTTON("Log In", id='login-btn', type="button")
-        form = tf.FORM(id="login_form")
+        username = tf.INPUT(**{ 'type':"TEXT", 'id':'username', 'name':"username", 'placeholder':"Username", 'data-bind':'value: username' })
+        password = tf.INPUT(**{ 'type':"PASSWORD", 'id':'password', 'name':"password", 'placeholder':"Password", 'data-bind':'value: password'})
+        submit = tf.BUTTON("Log In", **{'id':'login-btn', 'data-bind':'click: clicked'})
+        form = tf.FORM(id = "login_form")
         form.fields = [username, password, submit]
         container.form = form
         container.msg = tf.SPAN(id="login-msg")
-        container.script = tf.SCRIPT(open("fe/src/js/login.js").read(), escape=False)
+        container.script = tf.SCRIPT(open("fe/src/js/login.js").read(), escape=False, type="text/javascript", language="javascript")
         return container
 
 class InvoicingPage(BasePage):
