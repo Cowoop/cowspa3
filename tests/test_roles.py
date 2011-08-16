9d753458bbe6999f85a7b8dfa89b0dad6b481ca6
Shekhar <pythonic@gmail.com>
2011-08-16 23:48:11 +0530

    role tests

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -5,6 +5,10 @@ import be.apis.role as rolelib
 import be.apis.user as userlib
 from nose.tools import nottest
 
+# dependencies member
+role_data = dict(context = 'BizPlace:1', roles = ['director', 'host'], user_id = 1)
+role_map = dict(director='Director', host='Host')
+
 def setup():
     commontest.setup_test_env()
     env.context.pgcursor.connection.commit()
@@ -12,3 +16,15 @@ def setup():
 def test_create_superuser():
     user_id = userlib.create_superuser(**test_data.super_user)
     assert rolelib.get_user_roles(user_id)['global'] == ["Admin"] 
+
+def test_assign_role():
+    rolelib.assign(**role_data)
+    roles_dict = rolelib.get_user_roles()
+    assert set(role_map.values()).issubset(roles_dict[test_data['bizplace']['name']]) == True
+    env.context.pgcursor.connection.commit()
+
+def test_assign_role():
+    rolelib.revoke(**test_data.role_data)
+    roles_dict = rolelib.get_user_roles()
+    assert set(role_map.values()).intersection(roles_dict[test_data['bizplace']['name']]) = set()
+    env.context.pgcursor.connection.commit()
