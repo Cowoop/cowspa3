e1c99140eebbe3d74a641fd130ac793a76cb9343
Shekhar <pythonic@gmail.com>
2011-08-19 10:53:26 +0530

    bottombar 1st cut

--- fe/bases.py
+++ fe/bases.py
@@ -9,6 +9,7 @@ class CSPage(sphc.more.HTML5Page):
     current_tab = 'Home'
     css_links = ['/css/emastic-type.css', '/css/common.css', '/css/grid.css']
     jslibs = html5widget_libs + sphc.more.HTML5Page.jslibs + ['/js/json2.js', '/js/jquery.jsonrpc.js', '/js/common.js', '/js/knockout-1.2.1.js', '/js/jquery.cookie.js']
+    bottom_links = [('Twitter', 'http://twitter.com/cowspa'), ('API', '#API')]
 
 class CSAnonPage(CSPage):
     top_links = [('login', '/login')]
@@ -31,7 +32,7 @@ booking_opt = [
 invoicing_opt = None
 
 class CSAuthedPage(CSPage):
-    top_links = [('account', '/account'), ('logout', '/logout')]
+    top_links = [('Account', '/account'), ('Theme', '#themes'), ('Logout', '/logout')]
     nav_links = [
         ('Dashboard', '#/dashboard', None),
         ('Profile', '#/profile', None),
@@ -55,3 +56,13 @@ class CSAuthedPage(CSPage):
         topbar.logo = product_name
         topbar.links = links
         return topbar
+    def bottombar(self):
+        bar = tf.DIV(Class='bottombar')
+        links = []
+        for label, link in self.bottom_links[:-1]:
+            links.append(tf.A(label, href=link))
+            links.append(' | ')
+        last_link = self.bottom_links[-1]
+        links.append(tf.A(last_link[0], href=last_link[1]))
+        bar.links = links
+        return bar
