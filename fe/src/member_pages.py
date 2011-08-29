3a57dc44412d3c5a646487090be7560f4957542c
Shekhar <pythonic@gmail.com>
2011-08-29 11:22:54 +0530

    more explicit form coding

--- fe/src/member_pages.py
+++ fe/src/member_pages.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import sphc
+import sphc.more
 import fe.bases
 import pycountry
 import commonlib.shared.static_data as data_lists
@@ -61,7 +62,6 @@ class MemberCreate(BasePage):
 
         form  = tf.FORM(id="createmember_form")
         for field in fields:
-            # field.line = tf.BR()
             form.content = field
 
         container.form = form
@@ -72,25 +72,33 @@ class MemberCreate(BasePage):
 class MemberProfile(BasePage):
     current_nav = 'Profile'
     title = 'Profile'
+
     def content(self):
 
         container = tf.DIV()
+        container.title = tf.H1(Class="section-title data-display_name")
         #                                                 About me Form       
+        edit = tf.DIV(Class="edit-link-box")
+        edit.link = tf.A("Edit", id='edit-link', href='#about')
+        fields = [edit]
+
+        field_data = [('First name', 'first_name'),
+            ('Last name', 'last_name'),
+            ('Short description', 'short_description'),
+            ('Long description', 'long_description')]
+        for label, name in field_data:
+            field = tf.DIV(Class="field-container")
+            field.label = tf.DIV(label, Class="field-name")
+            field.value = tf.DIV(id=name, Class="field-value")
+            fields.append(field)
+        fields.append(sphc.more.clear())
+
+        view = tf.DIV(Class='profile-forms', id="about_view_form", style="display:none")
+        view.fields= fields
+
+        container.view = view
+
         field_list = ['First_Name','Last_Name','Short_Description','Long_Description','Organization']
-        fields = []
-
-        field = tf.DIV(align="right")
-        field.a = tf.A("Edit", id='edit-link', href='#about')
-        fields.append(field)
-
-        fields = get_static_fields(field_list, [], fields)
-
-        form  = tf.FORM(Class='profile-forms', id="about_view_form", style="display:none")
-        for field in fields:
-            field.line = tf.BR()
-            form.content = field
-
-        container.form = form
 
         input_type_text = ['First_Name','Last_Name','Organization']
         input_type_textarea = ['Short_Description','Long_Description']
@@ -103,7 +111,6 @@ class MemberProfile(BasePage):
 
         form  = tf.FORM(Class='profile-forms', id="about_edit_form", style="display:none")
         for field in fields:
-            field.line = tf.BR()
             form.content = field
 
         container.form = form
@@ -126,7 +133,6 @@ class MemberProfile(BasePage):
 
         form  = tf.FORM(Class='profile-forms', id="account_edit_form", style="display:none")
         for field in fields:
-            field.line = tf.BR()
             form.content = field
 
         container.form = form
@@ -144,7 +150,6 @@ class MemberProfile(BasePage):
 
         form  = tf.FORM(Class='profile-forms', id="social_view_form", style="display:none")
         for field in fields:
-            field.line = tf.BR()
             form.content = field
 
         container.form = form
@@ -159,7 +164,6 @@ class MemberProfile(BasePage):
 
         form  = tf.FORM(Class='profile-forms', id="social_edit_form", style="display:none")
         for field in fields:
-            field.line = tf.BR()
             form.content = field
 
         container.form = form
@@ -177,7 +181,6 @@ class MemberProfile(BasePage):
 
         form  = tf.FORM(Class='profile-forms', id="contact_view_form", style="display:none")
         for field in fields:
-            field.line = tf.BR()
             form.content = field
 
         container.form = form
@@ -194,11 +197,10 @@ class MemberProfile(BasePage):
 
         form  = tf.FORM(Class='profile-forms', id="contact_edit_form", style="display:none")
         for field in fields:
-            field.line = tf.BR()
             form.content = field
-        
+
         container.form = form
-        
+
         container.script = tf.SCRIPT(open("fe/src/js/member_profile.js").read(), escape=False, type="text/javascript", language="javascript")
         container.script = tf.SCRIPT(open("fe/src/js/common_form_methods.js").read(), escape=False, type="text/javascript", language="javascript")
         return container
@@ -206,25 +208,30 @@ class MemberProfile(BasePage):
 def get_static_fields(field_list, web_links, fields):
     
     for attr in field_list:
-        field = tf.DIV()
-        field.label = tf.LABEL(content = ' '.join(attr.split('_')))
+        field = tf.DIV(Class="field-container")
+        field.label = tf.DIV(content = ' '.join(attr.split('_')), Class="field-name")
         if attr in web_links:
             field.a = tf.A(id=attr.lower(), name=attr.lower(), href="")
         else:
-            field.a = tf.LABEL(id=attr.lower(), name=attr.lower())
+            field.a = tf.LABEL(id=attr.lower(), name=attr.lower(), Class="field-value")
         fields.append(field)
+    fields.append(sphc.more.clear())
     return fields
-            
+
 def get_editable_fields(field_list, input_type_text, input_type_list, input_type_textarea, fields):
-    
+
     for attr in field_list:
         field = tf.DIV()
         field.label = tf.LABEL(' '.join(attr.split('_')), FOR=attr.lower())
         if attr in input_type_text:
             if attr in ['Twitter', 'Facebook', 'Linkedin']:
-                field.input = tf.INPUT(type='text', id=attr.lower()+"-label", name=attr.lower()+"-label", placeholder="Label")
-                field.input = tf.INPUT(type='text', id=attr.lower()+"-url", name=attr.lower()+"-url", placeholder="URL")
-            else:    
+                placeholder = 'Eg. My ' + attr
+                field.input_section = tf.DIV(Class='input-section')
+                inputs = [
+                    tf.INPUT(type='text', id=attr.lower()+"-label", name=attr.lower()+"-label", placeholder=placeholder),
+                    tf.INPUT(type='text', id=attr.lower()+"-url", name=attr.lower()+"-url", placeholder="URL") ]
+                field.input_section.inputs = inputs
+            else:
                 field.input = tf.INPUT(type='text', id=attr.lower(), name=attr.lower())
         elif attr in input_type_textarea:
             field.input = tf.TEXTAREA(id=attr.lower(), name=attr.lower(), rows=2, cols=25)
--- fe/src/pages.py
+++ fe/src/pages.py
@@ -7,9 +7,8 @@ BasePage = fe.bases.CSAuthedPage
 
 class LoginPage(fe.bases.CSAnonPage):
     title = "Cowspa | Login"
-    def content(self):
+    def main(self):
         container = tf.DIV(Class="login-mainx")
-        container.script = tf.SCRIPT(open("fe/src/js/login.js").read(), escape=False, type="text/javascript", language="javascript")
         username = tf.INPUT(type="TEXT", id='username', name="username", placeholder="Username")
         password = tf.INPUT(type="PASSWORD", id='password', name="password", placeholder="Password")
         submit = tf.BUTTON("Log In", id='login-btn', type='button')
@@ -19,7 +18,8 @@ class LoginPage(fe.bases.CSAnonPage):
         container.formbox = formbox
         container.formbox.form = form
         container.msg = tf.SPAN(id="login-msg")
-        
+        container.script = tf.SCRIPT(open("fe/src/js/login.js").read(), escape=False, type="text/javascript")
+
         container.imgbox = tf.DIV(Class="login-img")
         container.imgbox.img = tf.IMG(src="/images/cow.png", width="50%%", height="50%%", align="left")
         container.clear = sphc.more.clear()
@@ -29,7 +29,7 @@ class LogoutPage(fe.bases.CSAnonPage):
     title = "Cowspa | Logout"
     def content(self):
         return tf.SCRIPT(open("fe/src/js/logout.js").read(), escape=False, type="text/javascript", language="javascript")
-        
+
 class InvoicingPage(BasePage):
     current_tab = 'invoicing'
     title = 'Invoicing'
