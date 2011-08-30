c7716eb19e30d1dc52f9b1ee10b9c890d60909c1
Shekhar <pythonic@gmail.com>
2011-08-30 21:46:02 +0530

    minor

--- fe/src/js/super_create.js
+++ fe/src/js/super_create.js
@@ -3,12 +3,12 @@ $('#save-btn').click(function () {
     var params = {}
     for(var i in inputs){
         params[inputs[i].name] = inputs[i].value;
-        }
+    };
     function success() {
         $('#CreateSuper-msg').html("<big>☑</big> Super User Created successfully.");
-        };
+    };
     function error() {
         $('#CreateSuper-msg').html("<big>Error in Super User Creation. Try again</big>");
-        };
+    };
     jsonrpc('member.create_admin', params, success, error);
-    });
+});
