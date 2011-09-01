21a438e894a1c355b109087bd519ebdaf778a54a
Shekhar <pythonic@gmail.com>
2011-09-01 16:42:56 +0530

    schema test case useful in refreshing db

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,10 +1,10 @@
 import commontest
 import be.repository.stores as stores
+import be.repository.access
 
 commontest.setup_test_simple_env()
 
 def test_create():
-    import be.repository.access
     assert bool(stores.known_stores) == True
     for store in stores.known_stores.values():
         store.setup()
@@ -14,3 +14,10 @@ def test_destroy():
     for store in stores.known_stores.values():
         store.destroy()
     env.context.pgcursor.connection.commit()
+
+def test_recreate():
+    for store in stores.known_stores.values():
+        store.setup()
+    env.context.pgcursor.connection.commit()
+
+
