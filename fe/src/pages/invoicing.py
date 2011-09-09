3feee50e8ac3a375f500d3750c085f738aa98f81
Shekhar <pythonic@gmail.com>
2011-09-09 11:40:35 +0530

    new invoice frontend and related styles

--- fe/src/pages/invoicing.py
+++ fe/src/pages/invoicing.py
@@ -5,62 +5,96 @@ import fe.bases
 tf = sphc.TagFactory()
 BasePage = fe.bases.CSAuthedPage
 
+class InvoiceBasePage(BasePage):
+    def search(self):
+        row = tf.DIV(Class="searchbox")
+        cell = tf.DIV()
+        cell.data = tf.SPAN("Invoice Search", Class="search-label", For="search")
+        row.cell = cell
+        cell = tf.DIV()
+        cell.data = tf.INPUT(id="search", Class="search-input", type="text")
+        row.cell = cell
+        return row
+
 class New(BasePage):
 
     title = "New Invoice"
+    current_nav = 'Invoicing'
 
     def content(self):
-        container = tf.DIV()
+        container = tf.DIV(id='invoice-form-container')
+        container.header = tf.DIV("INVOICE", id="header")
+        content = tf.DIV(id='invoice-content')
+        container.content = content
 
-        container.member_field = tf.INPUT(type="text", name="invoicee", placeholder="Invoicee")
-        container.member_info = tf.DIV(Class="invoicee-info")
-        container.po_number = tf.DIV()
-        container.po_number.label = tf.LABEL("P. O. Number", For="po_number")
-        container.po_number.input = tf.INPUT(type="text", name="po_number", placeholder="Purchase Order number")
-        container.notice = tf.DIV()
-        container.notice.label = tf.LABEL("Notice/Annoucement (only for this invoice)")
-        container.notice.text = tf.TEXTAREA(name="message")
+        content.col1 = tf.DIV(id='col1')
+        content.col2 = tf.DIV(id='col2')
 
-        usages = tf.TABLE()
+        content.col1.member_field = tf.INPUT(type="text", name="invoicee", id="invoicee-search", placeholder="Invoicee")
+        content.col1.member_info_tmpl = tf.DIV(id="invoicee-info-tmpl", Class="hidden")
+        content.col1.member_info_tmpl.m_id = tf.C("Membership id: ${id}")
+        content.col1.member_info_tmpl.br = tf.BR()
+        content.col1.member_info_tmpl.address = tf.PRE("${address}")
+        content.col1.member_info = tf.DIV(id="invoicee-info", Class="hidden")
+
+        info = tf.TABLE()
+        tr = tf.TR()
+        cell1 = tf.TD("Date")
+        cell2 = tf.TD()
+        cell2.input = tf.INPUT(type="date", value="11/11/2011")
+        tr.cells = [cell1, cell2]
+        info.tr = tr
+        tr = tf.TR()
+        cell1 = tf.TD("P. O. Number")
+        cell2 = tf.TD()
+        cell2.input = tf.INPUT(type="text", name="po_number", placeholder="Optional")
+        tr.cells = [cell1, cell2]
+        info.tr = tr
+        content.col2.info = info
+
+        content.notice = tf.DIV(Class="invoice-form-notice")
+        content.notice.label = tf.DIV("Optional: Notice/Annoucement (only for this invoice)", Class="note")
+        content.notice.text = tf.TEXTAREA(name="message", placeholder="Test")
+
+        usages = tf.TABLE(id='usages')
         usages.caption = tf.CAPTION("Usages")
         usages.header = tf.TR()
         usages.header.cells = [tf.TH(name) for name in ('Resource', 'Rate', 'Qty', 'Unit', 'Start-End', 'Total', 'Actions')]
 
-        usage_tmpl = tf.TR()
-        usage_tmpl.td = tf.TD('${resource_name}')
-        usage_tmpl.td = tf.TD('${rate}')
-        usage_tmpl.td = tf.TD('${qty}')
-        usage_tmpl.td = tf.TD('${unit}')
-        usage_tmpl.td = tf.TD('${start}')
-        usage_tmpl.td = tf.TD('${end}')
-        usage_tmpl.td = tf.TD('${total}')
+        content.usages = usages
 
-        add_usage_form = sphc.more.VForm(id='new-usage-form')
+        usage_tmpl = sphc.more.jq_tmpl("usage-tmpl")
+        usage_tmpl.tr = tf.TR()
+        usage_tmpl.tr.td = tf.TD('${resource_name}')
+        usage_tmpl.tr.td = tf.TD('${rate}')
+        usage_tmpl.tr.td = tf.TD('${quantity}')
+        usage_tmpl.tr.td = tf.TD('${unit}')
+        usage_tmpl.tr.td = tf.TD('${start} - ${end}')
+        usage_tmpl.tr.td = tf.TD('${total}')
+        usage_tmpl.tr.td = tf.TD('')
+
+        content.usage_tmpl = usage_tmpl
+
+        add_usage_form = sphc.more.VForm(id='new-usage-form', action='#')
         add_usage_form.add_field("Resource Name", tf.INPUT(name='resource_name', id='resource_name', nv_attrs=('required',), placeholder="Resource name"))
-        add_usage_form.add_field("Rate", tf.INPUT(name='name', id='name', nv_attrs=('required',), placeholder="eg. 12.00"), "Do not include currency")
+        add_usage_form.add_field("Rate", tf.INPUT(name='rate', id='rate', nv_attrs=('required',), placeholder="eg. 12.00"), "Do not include currency")
         add_usage_form.add_field("Quantity", tf.INPUT(name='quantity', id='quantity', nv_attrs=('required',), placeholder="eg. 10. Not applicable for time based resource"), fhelp="For non time based resources. Do not include unit")
+        add_usage_form.add_field("Unit", tf.INPUT(name='unit', id='unit', nv_attrs=('required',)))
         add_usage_form.add_field("Start", tf.INPUT(name='start', id='start', nv_attrs=('required',)))
         add_usage_form.add_field("End", tf.INPUT(name='end', id='end'), "Optional. Only for time based resources.")
+        add_usage_form.add_buttons(tf.INPUT(type="button", value="Add", id='submit-usage'))
         add_usage_section = tf.DIV(id='new-usage-section', Class='hidden')
         add_usage_section.form = add_usage_form.build()
 
-        new = tf.BUTTON("Add usage", id="new-usage-button")
-        script = tf.SCRIPT("""
-        $('#new-usage-form #start').datetimepicker({
-            timeFormat: 'h:m',
-            dateFormat: 'dd.mm.yy',
-        });
-        $('#new-usage-form #end').datetimepicker({
-            timeFormat: 'h:m',
-        });
-        $('#new-usage-button').click(function() {
-            $('#new-usage-form').dialog({ title: "Create new usage", width: 500});
-        });
-        """)
+        content.add_usage_form = add_usage_section
 
-        container.usages = usages
-        container.new = new
-        container.form = add_usage_section
-        container.script = script
+        invoice_buttons = tf.DIV(Class="invoice-buttons")
+        invoice_buttons.status = tf.DIV("Invoice is not saved yet.", id="action-status")
+        invoice_buttons.buttons = [ tf.BUTTON("Save", Class="big-button"),
+            tf.BUTTON("View (PDF)", Class="big-button"), tf.BUTTON("Send", Class="big-button")]
+
+        content.new = tf.BUTTON("Add usage", id="new-usage-button")
+        content.script = tf.SCRIPT(open("fe/src/js/new_invoice.js").read(), escape=False)
+        content.buttons = invoice_buttons
 
         return container
