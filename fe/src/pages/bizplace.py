43a18ac1512a0362c1ef4906bf41bd0ba33d03bf
Mandar Vaze <mandarvaze@gmail.com>
2011-10-20 19:28:30 +0530

    Added new js file (Missing from previous commit) Now Country is also shown in List Location Using new sphc method for cleaner code.

--- fe/src/pages/bizplace.py
+++ fe/src/pages/bizplace.py
@@ -47,12 +47,11 @@ class List(BasePage):
         location_tmpl = sphc.more.jq_tmpl('loc_tmpl')
         location_tmpl.box = tf.DIV(Class='location-box')
         location_tmpl.box.link = tf.A("${name}", id='edit-link_${id}', href='#edit', Class='location-title')
-        location_tmpl.box.city = tf.LABEL("${city}", Class='location-info')
+        location_tmpl.box.city = tf.LABEL("   ${city}, ${country}", Class='location-info')
         location_tmpl.box.short_description = tf.DIV("${short_description}", Class='location-info')
         container.loctmpl = location_tmpl
 
-        container.script = tf.SCRIPT(open("fe/src/js/list_locations.js").read(), 
-                                   escape=False, type="text/javascript", 
-                                   language="javascript")
+        container.script = sphc.more.script_fromfile("fe/src/js/list_locations.js")
+
         return container
 
