3514721859ad6a0cba395b9f3a611a2f77e56799
Shekhar <pythonic@gmail.com>
2011-11-03 16:23:10 +0530

    remoed dead code

--- be/apis/pricing.py
+++ be/apis/pricing.py
@@ -6,29 +6,6 @@ pricing_store = dbaccess.pricing_store
 
 class PricingCollection:
 
-    def new(self, plan_id, resource_prices):
-        """
-        resource_prices: a tuple containing resource_id, price and date from which the price will be activated
-        """
-        # check if a record  with same plan_id and starts exists
-        # multi insert
-        price_ids = []
-        for resource_id, amount, from_date in resource_prices:
-            pricing = dbaccess.get_resource_pricing(plan=plan_id, resource=resource_id)
-            if pricing.starts == from_date: # price change detected
-                # find all usages using this pricing
-                # warn user about such usages getting affected. Show invoiced and un-invoiced
-                # allow user to call same api with force_update_usages=1
-                pass
-            if pricing:
-                # end earlier price
-                pricing_store.update_by(crit=dict(plan=plan_id, resource=pricing.resource_id, start=pricing.starts,ends=pricing.ends), amount=amount)
-            # create new pricing
-            price_id = pricing_store.add(plan=plan_id, resource=resource_id, start=from_date, amount=amount)
-            price_ids.append(price_id)
-
-        return price_ids
-
     def new(self, resource_id, plan_id, starts, amount):
         # starts should be greater than last pricings start which has no 'ends' date yet
         old_pricings = pricing_store.get_by(crit=dict(plan=plan_id, resource=resource_id, ends=None))
