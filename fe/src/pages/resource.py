874927b276a42d4fc5c05e54959add95112a9a3c
Shekhar <pythonic@gmail.com>
2011-11-16 19:10:31 +0530

    comsetic changes to make resource list look better

--- fe/src/pages/resource.py
+++ fe/src/pages/resource.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import sphc
+import sphc.more
 import fe.bases
 import commonlib.shared.static as static
 
@@ -30,34 +31,32 @@ class ResourceCreate(BasePage):
         container.form = form.build()
         container.script = sphc.more.script_fromfile("fe/src/js/resource_create.js")
         return container
-        
+
 class ResourceManage(BasePage):
     current_tab = 'Resources'
     title = 'Manage resources'
     def content(self):
         container = tf.DIV()
-        
+
         types = tf.DIV("Types : ", id="resource_types", Class="resource_types")
         for rtype in static.resource_types:
             types.type = tf.INPUT(value=rtype['label'], type="Button", Class="resource_type-hide", id="rtype_"+rtype['name'])
-        
+
         filters = tf.DIV("Filters : ", id="resource_filters", Class="resource_filters")
         filters.attr1 = tf.INPUT(value="Enabled", type="Button", Class="resource_filter-hide", id="enabled")
         filters.attr2 = tf.INPUT(value="Host Only", type="Button", Class="resource_filter-hide", id="host_only")
         filters.attr3 = tf.INPUT(value="Repairs", type="Button", Class="resource_filter-hide", id="repairs")
-        
-        resource_list = tf.DIV(Class='resource-list', id="resource_list")
+
+        resource_list = tf.DIV(Class='list-boxes', id="resource_list")
         resource_tmpl = sphc.more.jq_tmpl("resource-tmpl")
-        resource_tmpl.box = tf.DIV(Class='resource-box resource-hidden filtered_resource-visible typed_resource-hidden', id="resource_${id}")
-        resource_tmpl.box.first = tf.DIV(Class='resource-image_part')
-        resource_tmpl.box.first.picture = tf.IMG(Class='resource_list-logo', src="${picture}", id="picture_${id}", onerror="ImageNotAvailable(this);")
+        resource_tmpl.box = tf.DIV(Class='row resource-hidden filtered_resource-visible typed_resource-hidden', id="resource_${id}")
         resource_tmpl.box.second = tf.DIV(Class='resource-data_part')
         resource_tmpl.box.second.name = tf.DIV()
-        resource_tmpl.box.second.name.link = tf.A("${name}", id="edit_${id}", href="#/${id}/edit", Class="edit-link")
+        resource_tmpl.box.second.name.link = tf.A("${name}", id="edit_${id}", href="#/${id}/edit", Class="row-title")
         resource_tmpl.box.second.description = tf.DIV("${short_description}", id="short_description_${id}")
         resource_tmpl.box.third = tf.DIV(Class='resource-filter_part')
-        resource_tmpl.box.third.clock = tf.H2("◴", title="Time Based", id="clock_${id}", escape="false")
-        
+        resource_tmpl.box.third.clock = tf.C("◴", title="Time Based", id="clock_${id}", Class="text-xl", escape="false")
+
         resource_edit_form = sphc.more.Form(id='resource_edit_form', classes=['hform'])
         resource_edit_form.add_field("Name", tf.INPUT(id="name", type="text"))
         resource_type_list = tf.SELECT(id="type")
@@ -79,15 +78,15 @@ class ResourceManage(BasePage):
         resource_states.state3 = tf.INPUT(id="state_repairs", type="checkbox")
         resource_states.label3 = tf.C("Repairs")
         resource_edit_form.add_field("", resource_states)
-        resource_edit_form.add_buttons(tf.INPUT(type="button", value="Upadate", id='update_resource-btn'), tf.INPUT(type="button", value="Cancel", id='cancel-btn'))
+        resource_edit_form.add_buttons(tf.INPUT(type="button", value="Update", id='update_resource-btn'), tf.INPUT(type="button", value="Cancel", id='cancel-btn'))
         resource_edit = tf.DIV(id="resource_edit", Class='hidden')
         resource_edit.form = resource_edit_form.build()
-        
+
         container.types = types
         container.filters = filters
         container.resource_tmpl = resource_tmpl
         container.resource_list = resource_list
         container.resource_edit = resource_edit
-        
-        container.script = tf.SCRIPT(open("fe/src/js/resource_manage.js").read(), escape=False, type="text/javascript", language="javascript")     
+
+        container.script = sphc.more.script_fromfile("fe/src/js/resource_manage.js")
         return container
