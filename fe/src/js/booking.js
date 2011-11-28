90fbda065299da7506be7744781137a247263348
Shekhar <pythonic@gmail.com>
2011-11-28 21:24:47 +0530

    booking ui improvements

--- fe/src/js/booking.js
+++ fe/src/js/booking.js
@@ -1,8 +1,37 @@
-$('#booking-date-inp-vis').datepicker( {
-    altFormat: 'yy-mm-dd',
-    altField: '#booking-date-inp',
-    dateFormat: 'M d, yy'
+function sevendays(adate) {
+    var days = [];
+    for (var i=0;  i<= 6; i++) {
+        days[i] = new Date(adate.getTime() + (24 * 60 * 60 * 1000));
+    };
+    return days;
+};
+
+function set_day_titles() {
+    var date_selected = new Date($('#booking-date-inp').val());
+    $('.day-title').each( function (idx) {
+        var date_next = new Date(date_selected.getTime() + ((idx - 3) * 24 * 60 * 60 * 1000));
+        var date_text = $.datepicker.formatDate('D d', date_next)
+        $(this).text(date_text);
+    });
+};
+
+function error(resp) {
+    alert('Remote error: ' + resp.result.message);
+};
+
+function on_available_resources(resp) {
+    tariffs = resp.result;
+    $('#resource-opt').tmpl(tariffs).appendTo('#resource-select');
+};
+
+var params = {'owner': current_ctx};
+jsonrpc("resources.available", params, on_available_resources, error);
+
+$('#booking-date-inp').datepicker( {
+    onSelect: set_day_titles
 });
+
+set_day_titles();
 
 function init_cal() {
     $(".cal-day").selectable({
@@ -10,6 +39,6 @@ function init_cal() {
     });
 };
 
-$('#booking-date-inp').change ( function () {
+$('#booking-date-inp-vis').change ( function () {
     alert(1);
 });
--- fe/src/pages/booking.py
+++ fe/src/pages/booking.py
@@ -4,7 +4,7 @@ import fe.bases
 tf = sphc.TagFactory()
 BasePage = fe.bases.CSAuthedPage
 
-min15 = lambda: tf.DIV(Class='cal-min15')
+min15 = lambda x, y: tf.DIV(Class='cal-min15', id="slot_%s-%s" % (x,y))
 
 def hourrange(start, end):
     return ((((hr or 12) if hr < 12 else ((hr - 12) or hr)), ('AM' if hr < 12 else 'PM')) for hr in range(start, end))
@@ -12,19 +12,19 @@ def hourrange(start, end):
 def timecolumn(start, end):
     return tf.DIV([tf.DIV('%d %s' % hr, Class='cal-hour') for hr in hourrange(start, end)], Class='cal-day')
 
-def day():
-    slots = [min15() for i in range(24*4)] # 4 15mins * 24 hours
+def day(day_no):
+    slots = [min15(day_no, i) for i in range(24*4)] # 4 15mins * 24 hours
     for i, slot in enumerate(slots):
         remainder = (i%4)
         if not remainder:
             slot.add_classes(['hour-start'])
         elif remainder == 3:
             slot.add_classes(['hour-end'])
-    cells = [tf.DIV("title", Class='day-title', id="day-${day_id}")] + slots
-    return tf.DIV(cells, Class='cal-day')
+    cells = [tf.DIV("title", Class='day-title', id="day-title_%s" % day_no)] + slots
+    return tf.DIV(cells, Class='cal-day', id="day_%s" % day_no)
 
 def week():
-    return [tf.DIV([timecolumn(0, 24)] + [day() for x in range(7)], Class='cal-week')]
+    return [tf.DIV([timecolumn(0, 24)] + [day(i) for i in range(7)], Class='cal-week')]
 
 class Booking(BasePage):
     current_nav = 'Bookings'
@@ -35,11 +35,10 @@ class Booking(BasePage):
 
         resource_pane = tf.DIV(id="pane-resource")
         resource_pane.resource_opt = sphc.more.jq_tmpl('resource-opt')
-        resource_pane.resource_opt.opt = tf.OPTION()
+        resource_pane.resource_opt.opt = tf.OPTION("${name}", value="${id}")
+        resource_pane.date = tf.SPAN(type="text", id="booking-date-inp")
         resource_pane.select = tf.SELECT(id='resource-select')
         resource_pane.select.option = tf.OPTION("Select a resource", disabled="true", selected="selected")
-        resource_pane.date_vis = tf.SPAN(type="text", id="booking-date-inp-vis", placeholder="Select booking date")
-        resource_pane.date = tf.INPUT(type="hidden", id="booking-date-inp")
 
         booking_pane = tf.DIV(id="pane-booking")
         booking_pane.topbar = tf.DIV(id="booking-menu")
