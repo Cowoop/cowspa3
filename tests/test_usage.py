dc63725e5af365c7a591f85e08bcf05fdf579954
Shekhar <pythonic@gmail.com>
2011-12-02 13:34:02 +0530

    usage id was hard coded. fixed

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -16,17 +16,17 @@ def test_add_usage():
     data['resource_id'] = test_data.resource_id
     data['start_time'] = datetime.datetime.now().isoformat()
     data['end_time'] = datetime.datetime.now().isoformat()
-    usage_id = usagelib.usage_collection.new(**data)
+    test_data.usage_id = usagelib.usage_collection.new(**data)
     env.context.pgcursor.connection.commit()
-    assert usage_id == 1
+    assert isinstance(test_data.usage_id, (int, long))
 
 def test_update_usage():
-    old_cost = usagelib.usage_resource.get(1,'cost')
+    old_cost = usagelib.usage_resource.get(test_data.usage_id, 'cost')
     new_cost = 2000
     mod_data = dict(cost=new_cost)
-    usagelib.usage_resource.update(1, **mod_data)
+    usagelib.usage_resource.update(test_data.usage_id, **mod_data)
     assert old_cost == test_data.usage['cost']
-    assert new_cost == usagelib.usage_resource.info(1)['cost']
+    assert new_cost == usagelib.usage_resource.info(test_data.usage_id)['cost']
 
 def test_add_more_usage():
     for data in test_data.more_usages:
@@ -43,11 +43,11 @@ def test_find_by():
     assert len(usages) >= 4
     for usage in usages:
         assert usage['start_time'] >= data['start'] and usage['start_time'] <= data['end']
-    
+
     data = dict(res_owner_ids=[test_data.bizplace_id])
     usages = usagelib.usage_collection.find(**data)
     assert bool(usages)
-    
+
 def test_delete_usage():
     ret = usagelib.usage_collection.delete(1)
     assert ret == True
