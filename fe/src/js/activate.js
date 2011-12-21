db146e2318071adff0087aac6a4a4cafb44db657
Gaurav Rai <gaurav.hub@gmail.com>
2011-12-21 14:48:09 +0530

    Cookie setting on activation page

--- fe/src/js/activate.js
+++ fe/src/js/activate.js
@@ -6,9 +6,18 @@ function login() {
         params[inputs[i].name] = inputs[i].value;
     };
     function success(resp) {
-        action_status.text("Login is successful.").attr('class', 'status-success');
-        set_cookie("member_name", $.cookie("member_name").replace(/"/g, ""));
-        window.location = "/"+resp['result']['language']+"/"+resp['result']['theme']+"/dashboard";
+        action_status.text("Login is successful.").attr('class', 'status-success');  
+        set_cookie('authcookie', resp.result.auth_token)
+        set_cookie('user_id', resp.result.id)
+        set_cookie('roles', resp.result.roles)
+        set_member_name(resp.result.name);
+        var lang = resp.result.pref.language.split('_')[0];
+        // Following is hacked temporarily till we support build for more langs
+        if ($.inArray(lang, ['en','de']) == -1 ) {
+            lang = 'en'; // Build for this lang not available, return English as default
+        }
+//        var lang = resp.result.pref.language
+        window.location = "/" + lang + "/" + resp.result.pref.theme + "/dashboard";
         };
     function error() {
         action_status.text("Authentication Error. Try again").attr('class', 'status-fail');
