32817237e1da7d72b21a90d14d142a5c9af8e646
Shekhar <pythonic@gmail.com>
2011-12-28 16:11:31 +0530

    comsmetic changes

--- fe/src/pages/taxes.py
+++ fe/src/pages/taxes.py
@@ -11,19 +11,20 @@ class Taxes(BasePage):
     def content(self):
         container = tf.DIV()
         taxes = sphc.more.Form(id="taxation", Class="hform")
-        taxes.add_field("",tf.DIV([tf.INPUT(id="taxes_included", type="checkbox"), tf.label("Taxes are included")]))
+        taxes.add_field("Prices include Taxes", tf.INPUT(id="taxes_included", type="checkbox"), \
+            "If unchecked tax amount will be added to final usage prices")
         add_tax = tf.DIV(id="add_tax")
         add_tax.name = tf.DIV(tf.INPUT(placeholder="Tax name", type="text", id='new_tax'), Class='tax-name')
         add_tax.value = tf.DIV([tf.INPUT(placeholder="Value", type="number", id='new_value', step="0.1"), tf.span("%")], Class='tax-value')
         add_tax.action = tf.DIV(tf.BUTTON("Add", type="button", id="add_tax-btn"), Class="tax-delete")
-        taxes.add_field("", tf.DIV(add_tax, id="taxes_list"))
+        taxes.add_field("Taxes", tf.DIV(add_tax, id="taxes_list"))
         taxes.add_buttons(tf.BUTTON("Save", id="save-btn", type="submit"))
         tax_template = sphc.more.jq_tmpl('tax_tmpl')
         tax_template.new_tax = tf.DIV(Class="new-tax")
         tax_template.new_tax.name = tf.DIV(tf.INPUT(type="text", value="${name}", Class="new-name").set_required(), Class='tax-name')
         tax_template.new_tax.value = tf.DIV([tf.INPUT(type="number", value="${value}", Class="new-value", step="0.1").set_required(), tf.span("%")], Class='tax-value')
         tax_template.new_tax.delete = tf.DIV(tf.A("X", href="#"), Class="tax-delete remove-tax")
-        
+
         container.taxes = taxes.build()
         container.template = tax_template
         container.script = sphc.more.script_fromfile("fe/src/js/taxes.js")
