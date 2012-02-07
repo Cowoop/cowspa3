85aeb6fcd95fba269dc690de7d77afb9fdc1d216
Shekhar <pythonic@gmail.com>
2012-02-07 17:21:18 +0530

    few minor changes

--- be/apis/messagecust.py
+++ be/apis/messagecust.py
@@ -5,13 +5,13 @@ messagecust_store = dbaccess.stores.messagecust_store
 def new(owner_id, name, content):
     return messagecust_store.new(owner=owner_id, name=name, content=content)
 
-def get(name, owner_id):
+def get(owner_id, name):
     return messagecust_store.get_one_by(crit=dict(owner=owner_id, name=name))
 
 def list(owner_id):
     return messagecust_store.get_by(crit=dict(owner=owner_id, name=name))
 
-def update(name, owner_id, content):
+def update(owner_id, name, content):
     #msg = get(name, owner_id)
     # TODO send notification with old content
     messagecust_store.update_by(crit=dict(owner=owner_id, name=name), content=content)
