13301d4879b1272502f9c141d93c965586624c59
Shekhar <pythonic@gmail.com>
2012-02-09 01:10:08 +0530

    little improvements but still WIP

--- be/apis/messagecust.py
+++ be/apis/messagecust.py
@@ -1,4 +1,6 @@
+import os.path
 import be.repository.access as dbaccess
+import commonlib.messaging.messages
 
 messagecust_store = dbaccess.stores.messagecust_store
 
@@ -6,12 +8,16 @@ def new(owner_id, name, content):
     return messagecust_store.new(owner=owner_id, name=name, content=content)
 
 def get(owner_id, name):
-    return messagecust_store.get_one_by(crit=dict(owner=owner_id, name=name))
+    return messagecust_store.get_one_by_safe(crit=dict(owner=owner_id, name=name)) or \
+        getattr(commonlib.messaging.messages, name).content_dict['plain']
 
-def list(owner_id):
+def list(owner_id): # TODO: support names
     return messagecust_store.get_by(crit=dict(owner=owner_id, name=name))
 
 def update(owner_id, name, content):
     #msg = get(name, owner_id)
     # TODO send notification with old content
-    messagecust_store.update_by(crit=dict(owner=owner_id, name=name), content=content)
+    if get(owner_id, name):
+        messagecust_store.update_by(crit=dict(owner=owner_id, name=name), content=content)
+    else:
+        new(owner_id, name, content)
