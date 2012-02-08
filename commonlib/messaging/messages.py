dc3cd0470369be53aab7bf6190879b8699d49052
Shekhar <pythonic@gmail.com>
2012-02-09 01:07:57 +0530

    support for mako style templates (necessary for migration) and huge improvement in message API

--- commonlib/messaging/messages.py
+++ commonlib/messaging/messages.py
@@ -1,19 +1,44 @@
-from jinja2 import Environment, FileSystemLoader
+import os.path
+import copy
+from jinja2 import Environment
 
-env = Environment(loader=FileSystemLoader('commonlib/messaging/templates'))
+tenv = Environment('<%', '%>', '${', '}', '%')
+template_path = 'commonlib/messaging/templates'
 
-def render(template, data):
-    template = env.get_template(template)
-    return template.render(**data)
+def render(content, data):
+    return tenv.from_string(content).render(**data)
+
+class MessageMeta(type):
+    def __new__(cls, name, bases, dct):
+        tmplname = dct['name']
+        if tmplname:
+            content = open(os.path.join(template_path, tmplname)).read()
+            content_dict = dict()
+            exec(content, {}, content_dict)
+            dct['content_dict'] = content_dict
+        return type.__new__(cls, name, bases, dct)
 
 class Message(object):
-    def __init__(self, path):
-        self.path = path
-    def build(self, data):
-        rendered = render(self.path, data)
-        message_dict = dict()
-        exec(rendered, {}, message_dict)
-        return message_dict
+    __metaclass__ = MessageMeta
+    name = ''
+    def __init__(self, macros_data={}, overrides={}):
+        self.macros_data = macros_data
+        self.overrides = overrides
+        self.message_dict = copy.copy(self.content_dict)
+    def build(self):
+        self.message_dict.update(self.overrides)
+        for k,v in self.message_dict.items():
+            if isinstance(v, (list, tuple)):
+                v = tuple(render(s, self.macros_data) for s in v)
+            else:
+                v = render(v, self.macros_data)
+            self.message_dict[k] = v
+    def email(self):
+        return env.mailer.send(**self.message_dict)
 
-activation = Message('activation')
-invitation = Message('invitation')
+class Activation(Message):
+    name = 'activation'
+class Invitation(Message):
+    name = 'invitation'
+class Invoice(Message):
+    name = 'invoice'
