f6116e80df2f21c86536929ff2a145e308630d37
Shekhar <pythonic@gmail.com>
2012-02-22 13:42:33 +0530

    no default country

--- fe/src/common.py
+++ fe/src/common.py
@@ -7,3 +7,5 @@ tf = sphc.TagFactory()
 country_options =  [ tf.OPTION(country['label'], value=country['name']) for country in data_lists.countries ]
 team_options =  [ tf.INPUT(role.label, type="checkbox", name="roles", value=role.name) for role in team_roles]
 tz_options =  [ tf.OPTION(tz['label'], value=tz['name']) for tz in data_lists.timezones ]
+
+country_options.insert(0, tf.OPTION('- Select One -'))
