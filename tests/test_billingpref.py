7b5a7bbf3189b91082b7763671dbd050fabdc85c
Shekhar <pythonic@gmail.com>
2012-03-28 16:01:59 +0530

    billingpref tests

---
+++ tests/test_billingpref.py
@@ -0,0 +1,20 @@
+import commontest
+import test_data
+
+import be.apis.billingpref as billingpreflib
+import be.apis.member as memberlib
+
+def test_set_custom_billing():
+    billto_member_id, billing_member_id = test_data.even_more_member_ids
+    billing_member_details = memberlib.member_resource.details(billing_member_id)
+    mode = 1
+    data = dict(name='Mandrake magic academy', address='Somewhere', city='Somecity')
+    billingpreflib.billingpref_resource.update(billing_member_id, mode=mode, details=data)
+    details = billingpreflib.billingpref_resource.get_details(billing_member_id)
+    for attr in ('name', 'address', 'city'):
+        assert data[attr] == details[attr]
+    assert billingpreflib.billingpref_resource.info(billing_member_id)['mode'] == 1
+
+def test_set_billto():
+    pass
+
