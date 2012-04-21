0319d95ea0416c510e8c5120770986221fdaba7b
Shekhar <pythonic@gmail.com>
2012-04-22 00:52:14 +0530

    helpful tool

---
+++ mygrep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $1 ]
+echo "Searching for \"$1\""
+then
+    find . -name '*.py' -type f -exec egrep -i -H "$1" '{}' \; | more
+    find fe/src/js -name '*.js' -type f -exec egrep -i -H "$1" '{}' \; | more
+else
+    echo "Usage: $0 pattern"
+fi
