87e8acdbb91e6006fd75ab474067cbebda6839f5
Shekhar <pythonic@gmail.com>
2012-04-30 18:36:39 +0530

    new api to find members by roles

--- fe/src/js/member_list.js
+++ fe/src/js/member_list.js
@@ -1,25 +1,32 @@
 function on_get_member_list_success(response) {
-	$('#member_table').dataTable({
-		"sDom": '<"H"lT>rt<"F"ip>',
-		"oTableTools": {
-		    "sSwfPath": "/swf/copy_cvs_xls_pdf"
-		},
-		"aaData": response.result,
-        "bJQueryUI": true,
-        "bDestroy": true,
-        "sPaginationType": "full_numbers",
-        "aaSorting": [[ 0, "asc" ]],
-        "aoColumns": [
-                { "sTitle": "Name", "sWidth": "25%" },
-                { "sTitle": "Membership No", "sWidth": "25%"},
-                { "sTitle": "Membership", "sWidth": "25%" },
-                { "sTitle": "Email", "sWidth": "25%",
-                    "fnRender": function(obj) {
-                        var email = obj.aData[obj.iDataColumn];
-                        return "<A href='mailto:"+email+"'>"+email+"</A>";
-                        }
-                }
-                ]
-	});
+    var data = response.result;
+    var aaData = [];
+    for (var i=0; i < data.length; i++) {
+        var item = data[i];
+        aaData.push([item.name, item.number, item.tariff_name, item.email]);
+    };
+    console.log(aaData);
+    $('#member_table').dataTable({
+    	"sDom": '<"H"lT>rt<"F"ip>',
+    	"oTableTools": {
+    	    "sSwfPath": "/swf/copy_cvs_xls_pdf"
+    	},
+    	"aaData": aaData,
+    "bJQueryUI": true,
+    "bDestroy": true,
+    "sPaginationType": "full_numbers",
+    "aaSorting": [[ 0, "asc" ]],
+    "aoColumns": [
+            { "sTitle": "Name", "sWidth": "25%" },
+            { "sTitle": "Membership No", "sWidth": "25%"},
+            { "sTitle": "Membership", "sWidth": "25%" },
+            { "sTitle": "Email", "sWidth": "25%",
+                "fnRender": function(obj) {
+                    var email = obj.aData[obj.iDataColumn];
+                    return "<A href='mailto:"+email+"'>"+email+"</A>";
+                    }
+            }
+            ]
+    });
 };
-jsonrpc('memberships.list_by_bizplace', {bizplace_id: current_ctx, hashrows: false}, on_get_member_list_success);
+jsonrpc('roles.members_by_roles', {context: current_ctx}, on_get_member_list_success);
