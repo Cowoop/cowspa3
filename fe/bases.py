6d27489246e44fb0637177ff54834d1f0928f445
Shekhar <pythonic@gmail.com>
2012-07-27 14:27:13 +0530

    knockout update

--- fe/bases.py
+++ fe/bases.py
@@ -12,7 +12,7 @@ class CSPage(sphc.more.HTML5Page):
     jslibs = ['/js/json2.js', '/js/jquery-1.7.2.min.js', '/js/jquery-ui.min.js',
             '/js/jQuery-Timepicker-Addon/jquery-ui-timepicker-addon.js',
             '/js/jquery.jsonrpc.js', '/js/jquery.cookie.js', '/js/moment.min.js',
-            '/js/jquery.autoSuggest.js', '/js/jquery.tmpl.js', '/js/knockout-2.0.0.js',
+            '/js/jquery.autoSuggest.js', '/js/jquery.tmpl.js', '/js/knockout-2.1.0.js',
             '/js/DataTables-1.9.0/media/js/jquery.dataTables.min.js',
             '/js/DataTables-1.9.0/extras/TableTools/media/js/ZeroClipboard.js',
             '/js/DataTables-1.9.0/extras/TableTools/media/js/TableTools.min.js',
